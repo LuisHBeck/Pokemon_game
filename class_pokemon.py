3864248117c6872b46b8ed3a1f09a1c41472ceb3
Luís Beck <125098484+LuisHBeck@users.noreply.github.com>
2023-05-17 14:54:48 -0300

    Add files via upload

--- class_pokemon.py
+++ class_pokemon.py
@@ -1,6 +1,15 @@
 from abc import ABC, abstractmethod
 from random import random
 import inquirer
+from time import sleep
+
+collors = {
+    'X': '\33[7;32m',
+    'Y': '\33[4;35m',
+    'Z': '\33[7;34m',
+    'N': '\33[7;31m',
+    'C': '\33[m'
+}
 
 class Pokemon():
     def __init__(self, name, life, attack, speed):
@@ -44,52 +53,70 @@ class Pokemon():
         pass
 
 
-    def power(poke_user, poke_pc):
-        if poke_user.type == "Watter" and poke_pc == "Fire":
-            poke_user.attack(2)
-        elif poke_user.type == "Fire" and poke_pc == "Grass":
-            poke_user.attack(2)
-        elif poke_user.type == "Grass" and poke_pc.type == "Watter":
-            poke_user.attack(2)
-        else:
-            poke_user.attack(0.5)
+    # def power(poke_user, poke_pc):
+    #     if poke_user.type == "Watter" and poke_pc == "Fire":
+    #         poke_user.attack(2)
+    #     elif poke_user.type == "Fire" and poke_pc == "Grass":
+    #         poke_user.attack(2)
+    #     elif poke_user.type == "Grass" and poke_pc.type == "Watter":
+    #         poke_user.attack(2)
+    #     else:
+    #         poke_user.attack(0.5)
 
-        if poke_pc.type == "Watter" and poke_user == "Fire":
-            poke_pc.attack(2)
-        elif poke_pc.type == "Fire" and poke_user == "Grass":
-            poke_pc.attack(2)
-        elif poke_pc.type == "Grass" and poke_user.type == "Watter":
-            poke_pc.attack(2)
-        else:
-            poke_pc.attack(0.5)
+    #     if poke_pc.type == "Watter" and poke_user == "Fire":
+    #         poke_pc.attack(2)
+    #     elif poke_pc.type == "Fire" and poke_user == "Grass":
+    #         poke_pc.attack(2)
+    #     elif poke_pc.type == "Grass" and poke_user.type == "Watter":
+    #         poke_pc.attack(2)
+    #     else:
+    #         poke_pc.attack(0.5)
 
 
     def attacking(poke_user, poke_pc):
         percentage = round(random(), 2)
 
+        Water.benefits(poke_user, poke_pc)
+        Fire.benefits(poke_user, poke_pc)
+        Grass.benefits(poke_user, poke_pc)
+
+        print(poke_user.attack)
+        print(poke_pc.attack)
+        
+
         if poke_user.speed > poke_pc.speed:
-            print(f"{poke_user.name} starts attacking!")
+            print(f"{collors['X']}{poke_user.name} {collors['C']} starts attacking!")
+            sleep(1)
             attack = poke_user.attack * percentage
             poke_pc.remove_life(attack)
-            print(f'{poke_user.name} attack = {attack:.2f}')
-            print(f'{poke_pc.name} life = {poke_pc.life:.2f}')
+            print(f"{collors['X']}{poke_user.name} {collors['C']} attack = {attack:.2f}")
+            sleep(1)
+            print(f'{collors["Z"]}{poke_pc.name}{collors["C"]} life = {poke_pc.life:.2f}')
+            sleep(1)
             print()
             print(f"Now its {poke_pc.name} turn!")
+            sleep(1)
             attack = poke_pc.attack * percentage
             poke_user.remove_life(attack)
-            print(f'{poke_pc.name} attack = {attack:.2f}')
-            print(f'{poke_user.name} life = {poke_user.life:.2f}')
+            print(f'{collors["Z"]}{poke_pc.name} {collors["C"]} attack = {attack:.2f}')
+            sleep(1)
+            print(f"{collors['X']}{poke_user.name} {collors['C']} life = {poke_user.life:.2f}")
+            sleep(1)
 
 
         else: 
-            print(f"{poke_pc.name} starts attacking!")
+            print(f'{collors["Z"]}{poke_pc.name} {collors["C"]} starts attacking!')
+            sleep(1)
             attack = poke_pc.attack * percentage
             poke_user.remove_life(attack)
-            print(f'{poke_pc.name} attack = {attack:.2f}')
-            print(f'{poke_user.name} life = {poke_user.life:.2f}')
+            print(f'{collors["Z"]}{poke_pc.name} {collors["C"]} attack = {attack:.2f}')
+            sleep(1)
+            print(f"{collors['X']}{poke_user.name} {collors['C']} life = {poke_user.life:.2f}")
+            sleep(1)
             
 
         while poke_user.life > 0 or poke_pc.life > 0:
+            print()
             game_resume = [
             inquirer.List('choice',
                             message="Select:",
@@ -99,31 +126,43 @@ class Pokemon():
             answers = inquirer.prompt(game_resume)
 
             if answers['choice'] == 'Run':
-                print(f'{poke_pc.name} won! You ran!!')
+                print(f'{collors["Z"]}{poke_pc.name} {collors["C"]} won! You ran!!')
+                sleep(1)
                 quit()
             else:
                 percentage = round(random(), 2)
-                print(f"Its {poke_user.name} turn")
+                print(f"Its {collors['X']}{poke_user.name} {collors['C']} turn")
+                sleep(1)
                 attack = poke_user.attack * percentage
                 poke_pc.remove_life(attack)
-                print(f'{poke_user.name} attack = {attack:.2f}')
+                print(f"{collors['X']}{poke_user.name} {collors['C']} attack = {attack:.2f}")
+                sleep(1)
                 if poke_pc.life < 0:
-                    print(f'{poke_pc.name} died')
-                    print(f"{poke_user.name} won! Congrats!")
+                    print(f'{collors["Z"]}{poke_pc.name} {collors["C"]} died')
+                    sleep(1)
+                    print(f"{collors['X']}{poke_user.name} {collors['C']} won! Congrats!")
+                    sleep(1)
                     quit()
-                print(f'{poke_pc.name} life = {poke_pc.life:.2f}')
+                print(f'{collors["Z"]}{poke_pc.name} {collors["C"]} life = {poke_pc.life:.2f}')
+                sleep(1)
                 print()
                 
                 percentage = round(random(), 2)
-                print(f"Now its {poke_pc.name} turn!")
+                print(f'Now its {collors["Z"]}{poke_pc.name} {collors["C"]} turn!')
+                sleep(1)
                 attack = poke_pc.attack * percentage
                 poke_user.remove_life(attack)
-                print(f'{poke_pc.name} attack = {attack:.2f}')
+                print(f'{collors["Z"]}{poke_pc.name} {collors["C"]} attack = {attack:.2f}')
+                sleep(1)
                 if poke_user.life < 0:
-                    print(f'{poke_user.name} died')
-                    print(f"{poke_pc.name} won! Congrats!")
+                    print(f"{collors['X']}{poke_user.name} {collors['C']} died")
+                    sleep(1)
+                    print()
+                    print(f'{collors["Z"]}{poke_pc.name} {collors["C"]} won! Congrats!')
+                    sleep(1)
                     quit()
-                print(f'{poke_user.name} life = {poke_user.life:.2f}')
+                print(f"{collors['X']}{poke_user.name} {collors['C']} life = {poke_user.life:.2f}")
+                sleep(1)
 
 
 class Grass(Pokemon):
@@ -131,9 +170,10 @@ class Grass(Pokemon):
         super().__init__(name, life, attack, speed)
         self.type = "Grass"
 
+    @staticmethod
     def benefits(self, poke_pc):
-        if poke_pc == "pce":
-            self._attack /= 2
+        if poke_pc.type == "Water":
+            self.attack /= 2
 
     
 class Fire(Pokemon):
@@ -141,9 +181,10 @@ class Fire(Pokemon):
         super().__init__(name, life, attack, speed)
         self.type = "Fire"
 
+    @staticmethod
     def benefits(self, poke_pc):
-        if poke_pc == "pcter":
-            self._attack /= 2
+        if poke_pc.type == "Grass":
+            self.attack /= 2
 
 
 class Water(Pokemon):
@@ -151,6 +192,7 @@ class Water(Pokemon):
         super().__init__(name, life, attack, speed)
         self.type = "Water"
 
+    @staticmethod
     def benefits(self, poke_pc):
-        if poke_pc == "Grass":
-            self._attack /= 2
+        if poke_pc.type == "Fire":
+            self.attack /= 2
--- pokemons.py
+++ pokemons.py
@@ -1,10 +1,10 @@
 from class_pokemon import Fire, Grass, Water, Pokemon
+from random import choice
 
 blastoise = Water("Blastoise", 79, 57, 78)
 charizard = Fire("Charizard", 78, 53, 100)
 venusaur = Grass("Venusaur", 78, 76, 80)
 
-Pokemon.attacking(charizard, venusaur)
-
-
 pokemons = [blastoise, charizard, venusaur]
+
+Pokemon.attacking(charizard, blastoise)
